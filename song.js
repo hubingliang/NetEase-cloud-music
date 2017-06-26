e9fe8b069a62c70cbb0ce6334526b3e8f7bcaedc
brown <1104524352@qq.com>
2017-06-26 15:07:42 +0800

    细节修复

--- song.js
+++ song.js
@@ -1,16 +1,9 @@
 let audio = document.createElement('audio')
 audio.src = 'http://os55j7nsx.bkt.clouddn.com/Timbo%20Mehrstein%20Gypsy%20Jazz%20Ensemble%20-%20Pour%20Toi.mp3'
-audio.play()
-audio.oncanplay = function() {
-    audio.play()
-    $('#play').css('display', 'none')
-    $('.cover').css('animation-play-state', '')
-    $('.disc_light').css('animation-play-state', '')
-}
 $('#cover').on('touchstart', function() {
     audio.pause()
-    $('.cover').css('animation-play-state', 'paused')
-    $('.disc_light').css('animation-play-state', 'paused')
+    $('.cover').addClass('norotate')
+    $('.disc_light').addClass('norotate')
     $('#play').css('display', '')
     $('.needle').removeClass('movein')
     $('.needle').addClass('moveout')
@@ -18,8 +11,8 @@ $('#cover').on('touchstart', function() {
 })
 $('#play').on('touchstart', function() {
     audio.play()
-    $('.cover').css('animation-play-state', '')
-    $('.disc_light').css('animation-play-state', '')
+    $('.cover').addClass('rotate').removeClass('norotate')
+    $('.disc_light').addClass('rotate').removeClass('norotate')
     $('#play').css('display', 'none')
     $('.needle').removeClass('moveout')
     $('.needle').addClass('movein')
