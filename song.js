f4e9c460d76f14a1fa68ed538cbda5b67694e891
brown <1104524352@qq.com>
2017-06-26 14:13:16 +0800

    地址

--- song.js
+++ song.js
@@ -1,5 +1,5 @@
 let audio = document.createElement('audio')
-audio.src = 'https://os5560mkh.bkt.clouddn.com/Timbo%20Mehrstein%20Gypsy%20Jazz%20Ensemble%20-%20Pour%20Toi.mp3'
+audio.src = 'http://os5560mkh.bkt.clouddn.com/Timbo%20Mehrstein%20Gypsy%20Jazz%20Ensemble%20-%20Pour%20Toi.mp3'
 audio.play()
 audio.oncanplay = function() {
     audio.play()
