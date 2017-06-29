592b9529f9dab596483733f741765bbe78c09761
brown <1104524352@qq.com>
2017-06-29 14:59:26 +0800

    1

--- song.js
+++ song.js
@@ -21,7 +21,7 @@ $('#play').on('touchstart', function() {
 
 $(function() {
     $.get('songs.json').then(function(object) {
-        console.log(object)
+        console.log(object[0].lyric)
         let { lyric } = object
         let array = lyric.split('\n')
         let regex = /^\[(.+)\](.*)$/
