fd8400708d6bf621843d52911a6b3d0dc3e567e4
brown <1104524352@qq.com>
2017-07-03 21:13:32 +0800

    tab功能添加

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
- setTimeout(function() {
-     $.get('./songs.json').then(function(response) {
-         let items = response
-         items.forEach(i => {
-             let $li = $(
-                 `
+ $(function() {
+     setTimeout(function() {
+         $.get('./songs.json').then(function(response) {
+             let items = response
+             items.forEach(i => {
+                 let $li = $(
+                     `
                 <a href="./song.html?id=${i.id}">
                         <div class="items">
                             <div class="left">
@@ -16,9 +17,91 @@
                         </div>
                     </a>
 					`
-             )
-             $('#lastestMusic').append($li)
+                 )
+                 $('#lastestMusic').append($li)
+             })
+             $('#lastestMusicLoading').remove()
          })
-         $('#lastestMusicLoading').remove()
+     }, 1000)
+
+
+
+
+
+     $('.nav').on('click', 'ol.tabItems>li', function(e) {
+         let $li = $(e.currentTarget).children().addClass('selected')
+         $li.parents().siblings().children().removeClass('selected')
+         let index = $(e.currentTarget).attr('number')
+         $('.content > .tabitem').eq(index).addClass('active').siblings().removeClass('active')
      })
- }, 1000)
+
+     $('.nav').on('tabChange', function(e, index) {
+         let $li = $('.content > .tabitem').eq(index)
+         if ($li.attr('data-downloaded') === 'yes') {
+             return
+         }
+         setTimeout(function() {
+             if (index === 1) {
+                 $.get('./page2.json').then((response) => {
+                     $li.text(response.content)
+                     $li.attr('data-downloaded', 'yes')
+                 })
+             } else if (index === 2) {
+                 return
+                 $.get('./page3.json').then((response) => {
+                     $li.text(response.content)
+                     $li.attr('data-downloaded', 'yes')
+                 })
+             }
+         }, 500)
+     })
+
+     let timer = undefined
+     $('input#searchSong').on('input', function(e) {
+         let $input = $(e.currentTarget)
+         let value = $input.val().trim()
+         if (value === '') { return }
+
+         if (timer) {
+             clearTimeout(timer)
+         }
+
+         timer = setTimeout(function() {
+             search(value).then((result) => {
+                 timer = undefined
+                 if (result.length !== 0) {
+                     $('#output').empty()
+                     let $ul = $('<ul></ul>')
+                     result.forEach((item) => {
+                         let $li = $(`<li><a href="/song.html?id=${item.id}">${item.name}</a></li>`)
+                         $li.appendTo($ul)
+                     })
+                     $('#output').append($ul)
+                 } else {
+                     $('#output').html('没有结果')
+                 }
+             })
+         }, 300)
+
+
+     })
+
+     function search(keyword) {
+         console.log('搜索' + keyword)
+         return new Promise((resolve, reject) => {
+             var database = [
+                 { "id": 1, "name": "那些花儿", },
+                 { "id": 2, "name": "情非得已", },
+                 { "id": 3, "name": "找自己", },
+             ]
+             let result = database.filter(function(item) {
+                 return item.name.indexOf(keyword) >= 0
+             })
+             setTimeout(function() {
+                 console.log('搜到' + keyword + '的结果')
+                 resolve(result)
+             }, (Math.random() * 200 + 1000))
+         })
+     }
+     window.search = search
+ })
